b7b73c5e4632631a3eae895ee721b842ebc560e9
Luke Bamtefa <41142438+heisluke@users.noreply.github.com>
2023-05-15 12:00:46 +0100

    firebase added

--- index.js
+++ index.js
@@ -9,5 +9,19 @@ const app = initializeApp(appSettings)
 const database = getDatabase(app)
 const weAreChampionsDB = ref(database, "shoppingList")
 
+const inputFieldEl = document.getElementById("endorse-input")
 const publishBtnEl = document.getElementById("publish-btn")
-const endorsementsSection = document.getElementById("endorsement")
+const endorsementSection = document.getElementById("endorsement-cards-holster")
+
+
+publishBtnEl.addEventListener("click", function() {
+    let inputValue = inputFieldEl.value
+    
+    push(weAreChampionsDB, inputValue)
+    
+    clearInputFieldEl()
+})
+
+function clearInputFieldEl() {
+    inputFieldEl.value = ""
+}
